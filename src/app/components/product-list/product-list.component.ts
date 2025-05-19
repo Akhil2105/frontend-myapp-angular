0d54c567052d89ed25675b7664c7d3451939cb9f
Akhil2105 <86476708+Akhil2105@users.noreply.github.com>
2025-05-18 20:01:11 -0400

    product search added after a short break

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,23 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductService, Product } from '../../services/product.service';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './product-list.component.html'
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
 
   constructor(private productService: ProductService) { }
+  searchTerm = '';
+  filteredProducts: Product[] = [];
 
   ngOnInit() {
     this.productService.getProducts().subscribe(data => {
       this.products = data;
+      this.filteredProducts = data; // Keep original + filtered list
     });
   }
+  onSearch() {
+    const term = this.searchTerm.toLowerCase().trim();
+    this.filteredProducts = this.products.filter(p =>
+      p.name.toLowerCase().includes(term) ||
+      p.price.toString().includes(term) ||
+      p.category.toLowerCase().includes(term)
+    );
+  }
+
   deleteProduct(id: number) {
     if (confirm('Are you sure you want to delete this product?')) {
       this.productService.deleteProduct(id).subscribe(() => {
